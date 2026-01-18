fdfc84b27ec944a276a76760f8df7887e623b5f7
nilesh kashani <nileshkashani45@gmail.com>
2026-01-18 17:58:58 +0530

    Update ProjectedRoute.jsx

--- src/components/ProjectedRoute.jsx
+++ src/components/ProjectedRoute.jsx
@@ -4,8 +4,13 @@ import { useAuth } from "@/contexts/authContext"
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
 
-  if (loading) return null
-  if (!user) return <Navigate to="/login" replace />
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
 
   return children
 }
